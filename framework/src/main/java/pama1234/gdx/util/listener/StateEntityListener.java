bc48c84c60d84bf6da57b24338e83205917b431e
pama1234 <zerunli20019@163.com>
2023-12-05 16:55:53 +0800

    update

--- framework/src/main/java/pama1234/gdx/util/listener/StateEntityListener.java
+++ framework/src/main/java/pama1234/gdx/util/listener/StateEntityListener.java
@@ -3,6 +3,16 @@ package pama1234.gdx.util.listener;
 import pama1234.gdx.util.wrapper.DisplayEntity.DisplayWithCam;
 
 public interface StateEntityListener<T> extends EntityListener,DisplayWithCam{
+  /**
+   * 到达这个实例
+   * 
+   * @param in 从这里离开
+   */
   public default void from(T in) {}
+  /**
+   * 离开这个实例
+   * 
+   * @param in 从这里到达
+   */
   public default void to(T in) {}
 }
