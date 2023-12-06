de8d9962813dfd5e910fcd1ea446dae906248430
pama1234 <zerunli20019@163.com>
2023-12-06 14:24:19 +0800

    update

--- framework/src/main/java/pama1234/gdx/util/wrapper/ScreenContentContainer.java
+++ framework/src/main/java/pama1234/gdx/util/wrapper/ScreenContentContainer.java
@@ -1,15 +1,47 @@
 package pama1234.gdx.util.wrapper;
 
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import pama1234.gdx.game.ui.element.TextField;
 import pama1234.gdx.util.app.UtilScreen;
 import pama1234.gdx.util.listener.EntityListener;
 import pama1234.gdx.util.listener.EntityNeoListener;
+import pama1234.util.Annotations.RedundantCache;
 import pama1234.util.listener.ServerEntityListener;
 import pama1234.util.wrapper.ServerEntityCenter;
 
+@RedundantCache
 public class ScreenContentContainer implements EntityNeoListener{
   public EntityCenter<UtilScreen,EntityListener> center;
   public EntityCenter<UtilScreen,EntityListener> centerCam;
   public EntityCenter<UtilScreen,EntityListener> centerScreen;
   public EntityNeoCenter<UtilScreen,EntityNeoListener> centerNeo;
   public ServerEntityCenter<ServerEntityListener> serverCenter;
+
+  public Stage screenStage,camStage;
+
+  public ScreenContentContainer initMember(UtilScreen p) {
+    center=new EntityCenter<UtilScreen,EntityListener>(p);
+    centerCam=new EntityCenter<UtilScreen,EntityListener>(p);
+    centerScreen=new EntityCenter<UtilScreen,EntityListener>(p);
+    centerNeo=new EntityNeoCenter<UtilScreen,EntityNeoListener>(p);
+    serverCenter=new ServerEntityCenter<ServerEntityListener>();
+    return this;
+  }
+
+  public void addScreenTextFields(TextField... in) {
+    for(TextField e:in) screenStage.addActor(e);
+  }
+  public void addCamTextFields(TextField... in) {
+    for(TextField e:in) camStage.addActor(e);
+  }
+  public void removeScreenTextFields(TextField... in) {
+    Group root=screenStage.getRoot();
+    for(TextField e:in) root.removeActor(e);
+  }
+  public void removeCamTextFields(TextField... in) {
+    Group root=camStage.getRoot();
+    for(TextField e:in) root.removeActor(e);
+  }
 }
--- framework/src/main/java/pama1234/gdx/util/wrapper/StateEntityBase.java
+++ framework/src/main/java/pama1234/gdx/util/wrapper/StateEntityBase.java
@@ -5,9 +5,14 @@ import pama1234.gdx.util.listener.StateEntityListener;
 
 public abstract class StateEntityBase<P extends UtilScreen,E extends StateEntityListener<?>,T extends StateEntityBase<P,E,?>>extends StateEntity<P,E> implements StateEntityListener<T>{
   public int id;
+  public ScreenContentContainer container;
   public StateEntityBase(P p) {
     super(p);
+    // initContainer(p);
     // displayCam=new DisplayEntity(this::displayCam);
+  }
+  public void initContainer(P p) {
+    container=new ScreenContentContainer().initMember(p);
   }
   public StateEntityBase(P p,int id) {
     this(p);
