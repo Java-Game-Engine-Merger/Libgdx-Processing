d37518b87805431fb9dab2db55d72c14ddc71d6e
pama1234 <pama1234@163.com>
2023-12-12 22:03:11 +0800

    update

--- framework/src/main/java/pama1234/gdx/util/wrapper/StateEntityBase.java
+++ framework/src/main/java/pama1234/gdx/util/wrapper/StateEntityBase.java
@@ -6,6 +6,8 @@ import pama1234.gdx.util.listener.StateEntityListener;
 public abstract class StateEntityBase<P extends UtilScreen,E extends StateEntityListener<?>,T extends StateEntityBase<P,E,?>>extends StateEntity<P,E> implements StateEntityListener<T>{
   public int id;
   public ScreenContentContainer container;
+  public boolean overlayType;
+  // public boolean underOverlay;
   public StateEntityBase(P p) {
     super(p);
     // initContainer(p);
@@ -18,4 +20,15 @@ public abstract class StateEntityBase<P extends UtilScreen,E extends StateEntity
     this(p);
     this.id=id;
   }
+  // @Override
+  // public void from(StateEntity0004 in) {
+  //   super.from(in);
+  //   // TODO 评估是否需要将centerScreen.refresh放到ScreenCoreState2D和3D中
+  //   p.refreshAllCenter();
+  // }
+  // @Override
+  // public void to(StateEntity0004 in) {
+  //   super.to(in);
+  //   p.refreshAllCenter();
+  // }
 }
