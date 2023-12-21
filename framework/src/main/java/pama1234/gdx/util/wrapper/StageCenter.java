22b7eb5d8cf609c1764d100b774507c549388e68
pama1234 <pama1234@163.com>
2023-12-21 09:43:26 +0800

    update

---
+++ framework/src/main/java/pama1234/gdx/util/wrapper/StageCenter.java
@@ -0,0 +1,10 @@
+package pama1234.gdx.util.wrapper;
+
+import pama1234.util.wrapper.Center;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+// TODO
+public class StageCenter extends Center<Stage>{
+
+}
