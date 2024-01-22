ea0187718faa4745bfff242bcd52f367e299c36a
pama1234 <pama1234@163.com>
2024-01-22 16:28:20 +0800

    update

--- server-framework/src/main/java/pama1234/util/wrapper/Center.java
+++ server-framework/src/main/java/pama1234/util/wrapper/Center.java
@@ -2,30 +2,10 @@ package pama1234.util.wrapper;
 
 import java.util.LinkedList;
 
-public class Center<T>{
-  public final LinkedList<T> list=new LinkedList<T>(),
-    add=new LinkedList<T>(),
-    remove=new LinkedList<T>();
-  public synchronized void refresh() {
-    list.addAll(add);
-    add.clear();
-    list.removeAll(remove);
-    remove.clear();
-  }
+public class Center<T> extends CenterAbstract<T,LinkedList<T>>{
 
-  public void addAll(T[] list) {
-    for(T e:list) add.add(e);
-  }
-  public void removeAll(T[] list) {
-    for(T e:list) remove.add(e);
-  }
-  public void add(T e) {
-    add.add(e);
-  }
-  public void remove(T e) {
-    remove.add(e);
-  }
-  public void listAdd(T e) {
-    list.add(e);
+  @Override
+  public LinkedList<T> createList() {
+    return new LinkedList<>();
   }
 }
