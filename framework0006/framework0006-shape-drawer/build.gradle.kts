833f56b971ad18e8a8e7520d642f508bb597b4a4
pama1234 <pama1234@163.com>
2024-03-18 13:27:37 +0800

    update on test for shape-p6

--- framework0006/framework0006-shape-drawer/build.gradle.kts
+++ framework0006/framework0006-shape-drawer/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
   id("java")
+  id("java-library")
+  id("maven-publish")
 }
-
-group = "org.example"
-version = "unspecified"
 
 repositories {
   mavenCentral()
@@ -20,3 +19,30 @@ dependencies {
 tasks.test {
   useJUnitPlatform()
 }
+
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      val pama1234PublishVersion: String by project
+      groupId = "pama1234"
+      artifactId = "p6-drawer"
+      version = "$pama1234PublishVersion"
+      from(components["java"])
+
+      pom {
+        name.set("pama1234-shape-p6")
+        description.set("shape drawer")
+        url.set("https://github.com/pama1234/just-some-other-libgdx-game")
+        scm {
+          url.set("https://github.com/pama1234/just-some-other-libgdx-game")
+        }
+      }
+    }
+  }
+}
+
+//java {
+//  sourceCompatibility = JavaVersion.VERSION_17
+//  targetCompatibility = JavaVersion.VERSION_17
+//}
