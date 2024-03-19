e3b80acd65e31d23669ed9e12d9d0b43afe94e49
pama1234 <zerunli20019@163.com>
2024-03-19 15:32:30 +0800

    draft on Pose3D(Vec3f rotate changed to Vec4f)

--- framework/src/test/java/pama1234/test/PoseTest3D.java
+++ framework/src/test/java/pama1234/test/PoseTest3D.java
@@ -1,7 +1,11 @@
 package pama1234.test;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.graphics.g3d.decals.Decal;
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector3;
 import org.junit.jupiter.api.Test;
+import pama1234.Tools;
 import pama1234.gdx.util.app.UtilScreen3D;
 import pama1234.gdx.util.launcher.MainAppBase;
 import pama1234.math.UtilMath;
@@ -34,7 +38,8 @@ public class PoseTest3D extends UtilScreen3D{
   @Override
   public void setup() {
     poseO=new Pose3D();
-    pose=new Pose3D(20,0,0,0, UtilMath.HALF_PI,0);
+    //    pose=new Pose3D(20,0,0,0,UtilMath.HALF_PI,0);
+    pose=new Pose3D(20,0,0);
     noStroke();
   }
 
@@ -45,6 +50,12 @@ public class PoseTest3D extends UtilScreen3D{
 
   @Override
   public void display() {
+    text(
+      Tools.cutToLastDigitString(UtilMath.deg(pose.rx()))+
+        " "+
+        Tools.cutToLastDigitString(UtilMath.deg(pose.ry()))+
+        " "+
+        Tools.cutToLastDigitString(UtilMath.deg(pose.rz())));
   }
 
   @Override
@@ -55,9 +66,10 @@ public class PoseTest3D extends UtilScreen3D{
     scircle(0,0,3);
     popMatrix();
 
-    fill(255,0,0);
+    fill(255,127,0);
 
     pushMatrix();
+    rotateToCam(pose);
     pose(pose);
     scircle(0,0,3);
     popMatrix();
@@ -67,5 +79,51 @@ public class PoseTest3D extends UtilScreen3D{
   @Override
   public void frameResized() {
 
+  }
+
+  Vector3 tmp=new Vector3(),tmp2=new Vector3(),dir=new Vector3(),tmp3=new Vector3();
+  Quaternion rotation=new Quaternion();
+  Vector3 up=new Vector3(0,1,0);
+  Vector3 dirX=new Vector3(1,0,0).nor(),
+    dirY=new Vector3(0,1,0),
+    dirZ=new Vector3(0,0,1);
+  public void rotateToCam(Pose3D pose) {
+    var pos=cam3d.camera.position;
+    //    var up=cam.up;
+
+    //        Decal d=null;
+    //        d.lookAt(cam.position,cam.up);
+
+    //    var tmp=new Vector3();
+    //    var tmp2=new Vector3();
+    //    var dir=new Vector3();
+    //    var rotation=new Quaternion();
+
+    //    var tmp3=new Vector3(pose.pos.x,pose.pos.y,pose.pos.z);
+    tmp3.set(
+      pose.pos.x,
+      pose.pos.y,
+      pose.pos.z);
+
+    dir.set(pos).sub(tmp3).nor();
+
+    tmp.set(up).crs(dir).nor();
+    tmp2.set(dir).crs(tmp).nor();
+
+    rotation.setFromAxes(true,
+      tmp.x,tmp2.x,dir.x,
+      tmp.y,tmp2.y,dir.y,
+      tmp.z,tmp2.z,dir.z);
+
+    pose.rotate.set(rotation.x,rotation.y,rotation.z,rotation.w);
+
+    //    pose.rotate.x=getAngleAround(dirX);
+    //    pose.rotate.y=getAngleAround(dirY);
+    //    pose.rotate.z=getAngleAround(dirZ);
+    //    println(pose.rotate);
+  }
+
+  private float getAngleAround(Vector3 dir) {
+    return rotation.getAngleAroundRad(dir);
   }
 }
--- server-framework/src/main/java/pama1234/math/transform/Pose3D.java
+++ server-framework/src/main/java/pama1234/math/transform/Pose3D.java
@@ -1,18 +1,20 @@
 package pama1234.math.transform;
 
 import pama1234.math.vec.Vec3f;
+import pama1234.math.vec.Vec4f;
 
 public class Pose3D{
   public Vec3f pos;
-  /** in rad format */
-  public Vec3f rotate;
+//  /** in rad format */
+//  public Vec3f rotate;
+  public Vec4f rotate;
   public Vec3f scale;
   public Pose3D() {
     this(0,0,0);
   }
   public Pose3D(float dx,float dy,float dz) {
     pos=new Vec3f(dx,dz,dy);
-    rotate=new Vec3f();
+    rotate= new Vec4f();
     scale=new Vec3f();
     scale=new Vec3f(1,1,1);
   }
@@ -20,7 +22,7 @@ public class Pose3D{
     float dx,float dy,float dz,
     float rx,float ry,float rz) {
     pos=new Vec3f(dx,dz,dy);
-    rotate=new Vec3f(rx,ry,rz);
+    rotate= new Vec4f(rx,ry,rz,0);
     scale=new Vec3f(1,1,1);
   }
   public Pose3D(
@@ -28,7 +30,7 @@ public class Pose3D{
     float rx,float ry,float rz,
     float sx,float sy,float sz) {
     pos=new Vec3f(dx,dz,dy);
-    rotate=new Vec3f(rx,ry,rz);
+    rotate= new Vec4f(rx,ry,rz,0);
     scale=new Vec3f(sx,sy,sz);
   }
   public float dx() {
@@ -58,4 +60,5 @@ public class Pose3D{
   public float sz() {
     return scale.z;
   }
+
 }
