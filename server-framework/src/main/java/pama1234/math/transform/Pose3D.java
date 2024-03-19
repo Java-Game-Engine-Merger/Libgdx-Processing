b571b17bb4d411d860175a151fe42ff999fa59d4
pama1234 <zerunli20019@163.com>
2024-03-19 13:25:14 +0800

    update

--- server-framework/src/main/java/pama1234/math/transform/Pose3D.java
+++ server-framework/src/main/java/pama1234/math/transform/Pose3D.java
@@ -4,16 +4,21 @@ import pama1234.math.vec.Vec3f;
 
 public class Pose3D{
   public Vec3f pos,rotate,scale;
+  public Pose3D() {
+    this(0,0,0);
+  }
   public Pose3D(float dx,float dy,float dz) {
     pos=new Vec3f(dx,dz,dy);
     rotate=new Vec3f();
     scale=new Vec3f();
+    scale=new Vec3f(1,1,1);
   }
   public Pose3D(
     float dx,float dy,float dz,
     float rx,float ry,float rz) {
     pos=new Vec3f(dx,dz,dy);
     rotate=new Vec3f(rx,ry,rz);
+    scale=new Vec3f(1,1,1);
   }
   public Pose3D(
     float dx,float dy,float dz,
