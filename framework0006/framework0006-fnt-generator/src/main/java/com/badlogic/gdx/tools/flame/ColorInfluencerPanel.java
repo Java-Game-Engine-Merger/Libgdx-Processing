81746bacd53fef4230c0df61ebd4392b5a354cb9
pama1234 <pama1234@163.com>
2024-04-02 19:00:41 +0800

    update(Hiero)

---
+++ framework0006/framework0006-fnt-generator/src/main/java/com/badlogic/gdx/tools/flame/ColorInfluencerPanel.java
@@ -0,0 +1,26 @@
+
+package com.badlogic.gdx.tools.flame;
+
+import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer;
+import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer.Single;
+
+/** @author Inferno */
+public class ColorInfluencerPanel extends InfluencerPanel<Single> {
+
+	GradientPanel tintPanel;
+	PercentagePanel alphaPanel;
+
+	public ColorInfluencerPanel (FlameMain particleEditor3D, ColorInfluencer.Single influencer) {
+		super(particleEditor3D, influencer, "Color Influencer", "Sets the particle color.");
+		initializeComponents(influencer);
+		setValue(influencer);
+	}
+
+	private void initializeComponents (ColorInfluencer.Single emitter) {
+		int i = 0;
+		addContent(i++, 0, tintPanel = new GradientPanel(editor, emitter.colorValue, "Tint", "", false));
+		addContent(i++, 0, alphaPanel = new PercentagePanel(editor, emitter.alphaValue, "Life", "Alpha", ""));
+		tintPanel.showContent(true);
+		alphaPanel.showContent(true);
+	}
+}
